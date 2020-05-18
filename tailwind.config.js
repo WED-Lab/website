9aa6483ddc13d3b11b9ec12553c43d01a8cec7d4
Neil Mispelaar <neil.mispelaar@outlook.com>
2020-05-18 15:22:46 -0400

    Add brand colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 // tailwind.config.js
 module.exports = {
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        'dark-turquoise': '#23BDC5',
+        'dark-red': '#DC3833',
+        'light-gray': '#f1f3f3'
+      }
+    }
+  },
   variants: {},
   plugins: [],
 }
