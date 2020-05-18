ce5549ba976ff1d4cb575c3337f08f5b3048b5d6
Neil Mispelaar <neil.mispelaar@outlook.com>
2020-05-18 16:24:16 -0400

    Add light turquoise brand colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        'light-turquoise': '#79CED8',
         'dark-turquoise': '#23BDC5',
         'dark-red': '#DC3833',
         'light-gray': '#f1f3f3'
