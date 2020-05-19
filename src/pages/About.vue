dc771973447f387fbbf3a67bd26a8c8a8bbe93a4
Neil Mispelaar <neil.mispelaar@outlook.com>
2020-05-19 12:32:21 -0400

    Adjust core element styles

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -3,9 +3,116 @@
     <main>
       <content-panel>
 
-        <h1 class="mb-5 text-5xl text-dark-turquoise font-extrabold">About</h1>
+        <h1>About</h1>
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+        <p>The concept of women's empowerment has received increased attention by the international development community in recent years. Yet, more work is needed to clarify what constitutes women's empowerment in contemporary circumstances, and how to evaluate various policies and practices for their impact on promoting various forms of women's empowerment.</p>
+
+        <p>The Women's Empowerment in Development (<abbr>WED</abbr>) Lab is housed at McGill University's Institute for the Study of International Development (<abbr>ISID</abbr>). It builds on the expertise of interdisciplinary researchers at <abbr>ISID</abbr>, in order to connect academics, practitioners and policy makers in Canada and globally on issues related to women's empowerment in developing countries.</p>
+
+        <h2 class="">Purpose of the Lab</h2>
+
+        <div class="flex justify-center mt-5">
+          <ul>
+            <li class="flex items-start">
+
+              <div class="bg-dark-turquoise rounded-full mt-3 p-2 fill-current text-white">
+                <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                  <path style="fill:#ffffff" fill="none" d="M9.917,0.875c-5.086,0-9.208,4.123-9.208,9.208c0,5.086,4.123,9.208,9.208,9.208s9.208-4.122,9.208-9.208
+                  C19.125,4.998,15.003,0.875,9.917,0.875z M9.917,18.141c-4.451,0-8.058-3.607-8.058-8.058s3.607-8.057,8.058-8.057
+                  c4.449,0,8.057,3.607,8.057,8.057S14.366,18.141,9.917,18.141z M13.851,6.794l-5.373,5.372L5.984,9.672
+                  c-0.219-0.219-0.575-0.219-0.795,0c-0.219,0.22-0.219,0.575,0,0.794l2.823,2.823c0.02,0.028,0.031,0.059,0.055,0.083
+                  c0.113,0.113,0.263,0.166,0.411,0.162c0.148,0.004,0.298-0.049,0.411-0.162c0.024-0.024,0.036-0.055,0.055-0.083l5.701-5.7
+                  c0.219-0.219,0.219-0.575,0-0.794C14.425,6.575,14.069,6.575,13.851,6.794z"></path>
+                </svg>
+              </div>
+
+              <div class="ml-5">
+                <h3>Advance knowledge</h3>
+
+                <p>Advance knowledge creation, dissemination and outreach on issues of women’s empowerment in global development.</p>
+              </div>
+
+            </li>
+            <li class="flex items-start mt-5">
+              <div class="bg-dark-turquoise rounded-full mt-3 p-2 fill-current text-white">
+
+                <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+
+                 <path style="fill:#ffffff" fill="none" d="M9.917,0.875c-5.086,0-9.208,4.123-9.208,9.208c0,5.086,4.123,9.208,9.208,9.208s9.208-4.122,9.208-9.208
+                  C19.125,4.998,15.003,0.875,9.917,0.875z M9.917,18.141c-4.451,0-8.058-3.607-8.058-8.058s3.607-8.057,8.058-8.057
+                  c4.449,0,8.057,3.607,8.057,8.057S14.366,18.141,9.917,18.141z M13.851,6.794l-5.373,5.372L5.984,9.672
+                  c-0.219-0.219-0.575-0.219-0.795,0c-0.219,0.22-0.219,0.575,0,0.794l2.823,2.823c0.02,0.028,0.031,0.059,0.055,0.083
+                  c0.113,0.113,0.263,0.166,0.411,0.162c0.148,0.004,0.298-0.049,0.411-0.162c0.024-0.024,0.036-0.055,0.055-0.083l5.701-5.7
+                  c0.219-0.219,0.219-0.575,0-0.794C14.425,6.575,14.069,6.575,13.851,6.794z"></path>
+                </svg>
+              </div>
+
+              <div class="ml-5">
+                <h3>Strengthen the evidence-base</h3>
+
+                <p>Strengthen the evidence-base on what works, and does not work, to empower women in developing countries.</p>
+              </div>
+            </li>
+
+            <li class="flex items-start mt-5">
+              <div class="bg-dark-turquoise rounded-full mt-3 p-2 fill-current text-white">
+                <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                  <path style="fill:#ffffff" fill="none" d="M9.917,0.875c-5.086,0-9.208,4.123-9.208,9.208c0,5.086,4.123,9.208,9.208,9.208s9.208-4.122,9.208-9.208
+                  C19.125,4.998,15.003,0.875,9.917,0.875z M9.917,18.141c-4.451,0-8.058-3.607-8.058-8.058s3.607-8.057,8.058-8.057
+                  c4.449,0,8.057,3.607,8.057,8.057S14.366,18.141,9.917,18.141z M13.851,6.794l-5.373,5.372L5.984,9.672
+                  c-0.219-0.219-0.575-0.219-0.795,0c-0.219,0.22-0.219,0.575,0,0.794l2.823,2.823c0.02,0.028,0.031,0.059,0.055,0.083
+                  c0.113,0.113,0.263,0.166,0.411,0.162c0.148,0.004,0.298-0.049,0.411-0.162c0.024-0.024,0.036-0.055,0.055-0.083l5.701-5.7
+                  c0.219-0.219,0.219-0.575,0-0.794C14.425,6.575,14.069,6.575,13.851,6.794z"></path>
+                </svg>
+              </div>
+
+              <div class="ml-5">
+                <h3>Understand the role of policy</h3>
+
+                <p>Understand how social and economic policy can help in providing real development solutions to eliminate global gender inequality.</p>
+              </div>
+
+            </li>
+
+            <li class="flex items-start mt-5">
+              <div class="bg-dark-turquoise rounded-full mt-3 p-2 fill-current text-white">
+                <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                  <path style="fill:#ffffff" fill="none" d="M9.917,0.875c-5.086,0-9.208,4.123-9.208,9.208c0,5.086,4.123,9.208,9.208,9.208s9.208-4.122,9.208-9.208
+                  C19.125,4.998,15.003,0.875,9.917,0.875z M9.917,18.141c-4.451,0-8.058-3.607-8.058-8.058s3.607-8.057,8.058-8.057
+                  c4.449,0,8.057,3.607,8.057,8.057S14.366,18.141,9.917,18.141z M13.851,6.794l-5.373,5.372L5.984,9.672
+                  c-0.219-0.219-0.575-0.219-0.795,0c-0.219,0.22-0.219,0.575,0,0.794l2.823,2.823c0.02,0.028,0.031,0.059,0.055,0.083
+                  c0.113,0.113,0.263,0.166,0.411,0.162c0.148,0.004,0.298-0.049,0.411-0.162c0.024-0.024,0.036-0.055,0.055-0.083l5.701-5.7
+                  c0.219-0.219,0.219-0.575,0-0.794C14.425,6.575,14.069,6.575,13.851,6.794z"></path>
+                </svg>
+              </div>
+
+              <div class="ml-5">
+                <h3>Enable experts</h3>
+
+                <p>Enable researchers, policy makers and practitioners, including CSOs and NGOs, to address global forms of gender inequality.</p>
+              </div>
+            </li>
+
+            <li class="flex items-start mt-5">
+              <div class="bg-dark-turquoise rounded-full mt-3 p-2 fill-current text-white">
+                <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                  <path style="fill:#ffffff" fill="none" d="M9.917,0.875c-5.086,0-9.208,4.123-9.208,9.208c0,5.086,4.123,9.208,9.208,9.208s9.208-4.122,9.208-9.208
+                  C19.125,4.998,15.003,0.875,9.917,0.875z M9.917,18.141c-4.451,0-8.058-3.607-8.058-8.058s3.607-8.057,8.058-8.057
+                  c4.449,0,8.057,3.607,8.057,8.057S14.366,18.141,9.917,18.141z M13.851,6.794l-5.373,5.372L5.984,9.672
+                  c-0.219-0.219-0.575-0.219-0.795,0c-0.219,0.22-0.219,0.575,0,0.794l2.823,2.823c0.02,0.028,0.031,0.059,0.055,0.083
+                  c0.113,0.113,0.263,0.166,0.411,0.162c0.148,0.004,0.298-0.049,0.411-0.162c0.024-0.024,0.036-0.055,0.055-0.083l5.701-5.7
+                  c0.219-0.219,0.219-0.575,0-0.794C14.425,6.575,14.069,6.575,13.851,6.794z"></path>
+                </svg>
+              </div>
+
+              <div class="ml-5">
+                <h3>Eliminate barriers</h3>
+
+                <p>Understand and eliminate the barriers that prevent women in developing countries from reaching their full potential, and from fully engaging with the economy, political systems and communities in which they live.</p>
+              </div>
+            </li>
+          </ul>
+        </div>
 
       </content-panel>
     </main>
