5c7e8647d7ab690565c9141a9bc55336618e0ecb
Neil Mispelaar <neil.mispelaar@outlook.com>
2020-05-22 13:16:20 -0400

    Fix focus dissappear

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -66,7 +66,7 @@
           <div class="block lg:hidden ml-auto">
             <button
               v-on:click="toggleMenu"
-              class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+              class="flex items-center px-3 py-2 border rounded focus:text-teal-200 text-teal-200 border-teal-400 hover:text-white hover:border-white">
               <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
             </button>
           </div>
