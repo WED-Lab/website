485b3f0c70d64b28dfede79826be904dbf5bb231
Neil Mispelaar <neil.mispelaar@outlook.com>
2020-06-06 16:03:00 -0400

    Adjust spacing in About page

--- src/components/home/About.vue
+++ src/components/home/About.vue
@@ -3,15 +3,15 @@
     <panel backgroundColor="bg-light-gray">
       <div class="w-full mx-auto">
         <div class="flex flex-col md:flex-row items-center">
-          <div class="w-3/4 md:w-1/2">
+          <div class="w-3/4 sm:w-9/12 md:hidden lg:block md:w-1/2">
             <g-image src="~/assets/images/brand/wed-lab-globe-large.png" alt="WEDLab Logo" />
           </div>
-          <div class="w-full md:w-1/2 ml-0 md:ml-10">
-            <h1 class="text-3xl md:text-4xl font-bold">Women's Empowerment in Development Lab</h1>
+          <div class="w-full lg:w-1/2 ml-0 md:ml-10">
+            <h2 class="mt-16 md:mt-0 text-4xl md:text-5xl md:text-center lg:text-left font-bold">Women's Empowerment in Development Lab</h2>
 
-            <p class="text-2xl my-10">This Research-to-Practice Lab is bridging the gap between academics, practitioners and policy makers in Canada and globally on issues related to women's empowerment in developing countries. The lab is led by Prof. Sonia Laszlo in partnership with McGill Institute for the Study of International Development</p>
+            <p class="mt-10 mb-10 md:text-center lg:text-left">This Research-to-Practice Lab is bridging the gap between academics, practitioners and policy makers in Canada and globally on issues related to women's empowerment in developing countries. The lab is led by Prof. Sonia Laszlo in partnership with McGill Institute for the Study of International Development</p>
 
-            <ul class="flex flex-col md:flex-row justify-between list-none">
+            <ul class="mt-16 flex flex-col md:flex-row justify-between list-none">
               <li>
                 <g-link
                   class="text-center block md:inline text-xl text-white bg-dark-turquoise rounded-lg py-2 px-5 font-bold hover:no-underline hover:bg-light-turquoise"
